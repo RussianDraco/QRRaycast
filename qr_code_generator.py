e08e8a31edf8022c8f14c7bc5ce6adbdccbcd009
unknown <dracospy369@gmail.com>
2025-01-12 16:09:12 -0500

    qr code python generator created

---
+++ qr_code_generator.py
@@ -0,0 +1,19 @@
+import qrcode
+
+with open("index.html", "r") as file:
+    html_content = file.read()
+
+qr = qrcode.QRCode(
+    version=40,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data(html_content)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+
+img.save("qr_code.png")
+img.show()
